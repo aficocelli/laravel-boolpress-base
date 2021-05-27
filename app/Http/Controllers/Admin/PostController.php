206b4fb6af9a4b7b9ffc6f7fd8632b63107c012d
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-27 15:39:21 +0200

    fix. bug delete (cascade)

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -132,8 +132,10 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy()
+    public function destroy(Post $post)
     {
-        
+        $post -> delete();
+
+        return redirect()->route('admin.posts.index');
     }
 }
