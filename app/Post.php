86aca83fc3e299a515333285638d6e3b69f41fb3
Alfredo Ficocelli <alfredo.ficocelli@gmail.com>
2021-05-27 17:04:55 +0200

    add. rel. many to many

--- app/Post.php
+++ app/Post.php
@@ -13,4 +13,9 @@ class Post extends Model
         return $this -> hasMany('App\Comment');
 
     }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 }
--- app/Tag.php
+++ app/Tag.php
@@ -8,5 +8,8 @@ class Tag extends Model
 {
     protected $guarded = [];
 
-    
+    public function posts()
+    {
+        return $this->belongsToMany('App\Post');
+    }
 }
